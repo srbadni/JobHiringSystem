13c9e53462e60e4dac1ee0a2f3c56c3c61436d5e
alireza sarabadani <srbadni@gmail.com>
2026-09-13 16:16:41 +0330

    save

---
+++ frontend/src/features/job-search-result/model/type.ts
@@ -0,0 +1,11 @@
+export enum SortType {
+    RELEVANCE = "relevance",
+    MOST_RECENT = "most_recent",
+    SALARY_DESC = "salary_desc",
+}
+
+export const SortTypeLabels = {
+    "relevance": "مرتبط ترین",
+    "most_recent": "جدیدترین",
+    "salary_desc": "بیشترین حقوق",
+}
